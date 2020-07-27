9b22c94589323e7c234dff90316ac8ae31da33de
Lynda Santiago <you@example.com>
2020-07-27 13:55:56 -0400

    passed the formState into the entire schema

--- watermyplants/src/components/SignInForm.js
+++ watermyplants/src/components/SignInForm.js
@@ -1,10 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import * as yup from 'yup'
 import {formSchema} from '../validation/formSchema'
 
 
 export default function SignInForm(){
 
+//created object with name and password and assigned to
+//variables below
   const initialFormState = {
       name: "",
       password: ""
@@ -13,11 +15,27 @@ export default function SignInForm(){
     name: "",
     password: ""
   }
-
+  
+  //passed in the variables inside of useState as an arguement
   const [formState, setFormState]=useState(initialFormState)
   const [errors, setErrors]=useState(initialErrors)
+  const [loginButtonDisabled, setLoginButtonDisabled]=useState(true)
 
 
+  // passed the formState into the formSchema to make sure
+  // all form input values are valid before the user is able to click submit
+  useEffect(()=>{
+    formSchema.isValid(formState).then(valid => {
+      setLoginButtonDisabled(!valid)
+    })
+  }, [formState])
+
+
+
+
+
+
+  
  return (
      <form>
          <h1>Login</h1>
@@ -27,7 +45,7 @@ export default function SignInForm(){
          <label htmlFor="password">
          <input type="password" id="password" name="name" placeholder="Password" />
          </label>
-         <button>LOGIN</button>
+         <button disabled={loginButtonDisabled}>LOGIN</button>
         
      </form>
  )
