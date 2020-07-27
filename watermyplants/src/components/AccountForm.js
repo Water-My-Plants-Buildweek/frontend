a0061bee1da33a86e477d5a5d0c8e240b7fecac8
Alex Williams <alexjwilliams1@gmail.com>
2020-07-27 11:11:10 -0700

    added some form info

--- watermyplants/src/components/AccountForm.js
+++ watermyplants/src/components/AccountForm.js
@@ -25,6 +25,22 @@ export default function AccountForm(props) {
                     placeholder='xxx-xxx-xxxx'
                     />
                 </label>
+                <label htmlFor='passwordInput'>
+                    <input 
+                    id = 'password'
+                    name = 'password'
+                    type = 'password'
+                    placeholder='Enter New Password'
+                    />
+                </label>
+                <label htmlFor='passwordConfirmInput'>
+                    <input 
+                    id = 'passwordConfirm'
+                    name = 'passwordConfirm'
+                    type = 'password'
+                    placeholder='Confirm New Password'
+                    />
+                </label>
 
                 <button className='submit-btn' disabled={disabled}>Submit</button>
             </form>
