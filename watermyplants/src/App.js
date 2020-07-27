707ed326286d9df975246b9dac5e825546dfaa25
Drew Ross <mail.drew.ross@gmail.com>
2020-07-27 13:48:04 -0700

    Remove ReduxRenderTest component from tree. Add message property to plantsReducer state to store success/error messages.

--- watermyplants/src/App.js
+++ watermyplants/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import ReduxRenderTest from './components/ReduxRenderTest';
 
 const App = () => {
   return (
     <div className="App">
-      <ReduxRenderTest />
+      
     </div>
   );
 }
