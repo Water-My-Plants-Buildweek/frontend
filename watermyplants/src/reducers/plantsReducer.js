1b26fa88f9f84203422967bccfb3385a8f7d067f
Drew Ross <mail.drew.ross@gmail.com>
2020-07-28 11:12:27 -0700

    Change image key to imageURL.

--- watermyplants/src/reducers/plantsReducer.js
+++ watermyplants/src/reducers/plantsReducer.js
@@ -7,21 +7,21 @@ const initialState = {
       nickname: 'Leaf Erikson',
       species: 'Arctic Thyme',
       h2oFrequency: 14,
-      image: 'https://images.unsplash.com/photo-1533038590840-1cde6e668a91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80'
+      imageURL: 'https://images.unsplash.com/photo-1533038590840-1cde6e668a91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80'
     },
     {
       id: 1,
       nickname: 'Po',
       species: 'Bamboo',
       h2oFrequency: 1,
-      image: 'https://images.unsplash.com/photo-1533038590840-1cde6e668a91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80'
+      imageURL: 'https://images.unsplash.com/photo-1533038590840-1cde6e668a91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80'
     },
     {
       id: 2,
       nickname: 'Penny',
       species: 'Pennywort Begonia',
       h2oFrequency: 14,
-      image: 'https://images.unsplash.com/photo-1533038590840-1cde6e668a91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80'
+      imageURL: 'https://images.unsplash.com/photo-1533038590840-1cde6e668a91?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2134&q=80'
     },
   ],
   message: ''
