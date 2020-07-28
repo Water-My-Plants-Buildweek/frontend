9a2ccd6f6f8a546cb9c3323a67a529bab4c2cc0c
Drew Ross <mail.drew.ross@gmail.com>
2020-07-28 12:37:13 -0700

    Implement axiosWithAuth into plantsActions. Remove unneeded actions and plantsReducer logic.

--- watermyplants/src/actions/plantsActions.js
+++ watermyplants/src/actions/plantsActions.js
@@ -1,16 +1,36 @@
-export const ADD_PLANT = 'ADD_PLANT';
-export const DELETE_PLANT = 'DELETE_PLANT';
-export const EDIT_PLANT = 'EDIT_PLANT';
+import { axiosWithAuth } from '../utils/axiosWithAuth';
+export const LOG_ERROR = 'LOG_ERROR';
+export const UPDATE_PLANTS = 'UPDATE_PLANTS';
 
-//These action creators will later become axios thunks when backend endpoints are working
-export const addPlant = plant => {
-  return { type: ADD_PLANT, payload: plant };
+export const addPlant = plant => dispatch => {
+  axiosWithAuth()
+    .post('/api/plants', plant)
+    .then(res => {
+      dispatch({ type: UPDATE_PLANTS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: LOG_ERROR, payload: err})
+    })
 };
 
-export const deletePlant = plantId => {
-  return { type: DELETE_PLANT, payload: plantId };
+export const deletePlant = plantId => dispatch => {
+  axiosWithAuth()
+    .post(`/api/plants/${plantId}`)
+    .then(res => {
+      dispatch({ type: UPDATE_PLANTS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: LOG_ERROR, payload: err})
+    })
 };
 
-export const editPlant = plant => {
-  return { type: EDIT_PLANT, payload: plant };
+export const editPlant = plant => dispatch => {
+  axiosWithAuth()
+    .put(`/api/plants/${plant.id}`, plant)
+    .then(res => {
+      dispatch({ type: UPDATE_PLANTS, payload: res.data });
+    })
+    .catch(err => {
+      dispatch({ type: LOG_ERROR, payload: err})
+    })
 };
