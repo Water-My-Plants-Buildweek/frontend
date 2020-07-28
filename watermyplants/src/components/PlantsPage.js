829e30a64dc5cb96c790769aba623ace171da402
Alex Williams <alexjwilliams1@gmail.com>
2020-07-28 13:49:08 -0700

    linked up to data

--- watermyplants/src/components/PlantsPage.js
+++ watermyplants/src/components/PlantsPage.js
@@ -2,9 +2,14 @@ import React, { useState, useEffect } from 'react'
 import PlantCard from './PlantCard'
 import NewPlantForm from './NewPlantForm'
 import { connect } from 'react-redux';
+import { getPlants } from '../actions/plantsActions'
 
 function PlantsPage(props) {
     
+    useEffect(() => {
+        props.getPlants()
+    },[])
+
     function onDelete(){
         ///add in delete logic
     }
@@ -36,4 +41,4 @@ const mapStateToProps = state => {
     }
 }
 
-export default connect(mapStateToProps, {})(PlantsPage);
+export default connect(mapStateToProps, {getPlants})(PlantsPage);
