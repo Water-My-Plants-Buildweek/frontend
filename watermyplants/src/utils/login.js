f09f437daa9be3285078418821b23850b98c002c
Drew Ross <mail.drew.ross@gmail.com>
2020-07-30 15:28:39 -0700

    Pull from master

--- watermyplants/src/utils/login.js
+++ watermyplants/src/utils/login.js
@@ -5,13 +5,9 @@ export function login(postObj, history) {
     .post("https://water-my-pants.herokuapp.com/api/auth/login", postObj)
     .then(async (response) => {
       await localStorage.setItem("token", response.data.token);
-<<<<<<< HEAD
       localStorage.setItem("id", response.data.id);
       localStorage.setItem("phone", response.data.phone);
       history.push('/plants');
-=======
-      history.push("/plants");
->>>>>>> e31853db978d9854d8edd34207df18391f428753
     })
     .catch((error) => {
       console.log("Error happend with the post request", error);
